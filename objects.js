0a32317524209d82be71b9634ea27c4e6a2fce0d
Star Prapakamol <billyaiyai@gmail.com>
2018-03-30 04:19:43 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,10 @@
 var playlist = {artist: song
 }
 
-function updatePlaylist ()
+function updatePlaylist (playlist,artist,song){
+  playlist.artist = 'song'
+}
+
+function removeFromPlaylist (playlist,artist){
+  delete playlist.artist
+}
